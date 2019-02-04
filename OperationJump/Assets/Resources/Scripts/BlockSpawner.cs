3109f20aee932b22f22c357a1cf56e0a2994be09
Matthew Chan <_mattc@hotmail.com>
2019-02-04 13:26:04 +0800

    Every third block spawned will be a long block

--- OperationJump/Assets/Resources/Scripts/BlockSpawner.cs
+++ OperationJump/Assets/Resources/Scripts/BlockSpawner.cs
@@ -13,11 +13,14 @@ public class BlockSpawner : MonoBehaviour
 	private float floorY = -8f;
 	private float spawnDistance = 2.4f;
 
+	private int blockSpawnCount;
+
 	private CameraController game;
 
 	void Start() {
 		game = GetComponent<CameraController>();
 		nextYSpawn = floorY + spawnDistance;
+		blockSpawnCount = 0;
 	}
 
 	void Update() {
@@ -27,10 +30,17 @@ public class BlockSpawner : MonoBehaviour
 				Vector3 newBlockPosition = new Vector3 (Random.Range(-3.75f + ((avgBlockLength + 0.5f) / 2), 3.75f - ((avgBlockLength + 0.5f) / 2)), nextYSpawn, 0);
 				GameObject newBlock = Instantiate(Block, newBlockPosition, Quaternion.identity);
 
-				Vector3 newBlockScale = new Vector3(Random.Range(avgBlockLength - 0.5f, avgBlockLength + 0.5f), 1, 1);
+				Vector3 newBlockScale;
+				if (blockSpawnCount % 3 != 0) {
+					newBlockScale = new Vector3(Random.Range(avgBlockLength - 0.5f, avgBlockLength + 0.5f), 1, 1);
+				} else {
+					newBlockScale = new Vector3(4, 1, 1);
+				}
+				
 				newBlock.transform.localScale = newBlockScale;
 
 				nextYSpawn += spawnDistance;
+				blockSpawnCount++;
 
 				// 10% chance to reduce avg block length by 0.25
 				if (Random.Range(0f, 1f) <= 0.1f) {
