f1c650e8d559578c40786c89e3584021cb418bb1
Matthew Chan <_mattc@hotmail.com>
2019-02-08 21:28:23 +0800

    Added moving blocks

---
+++ OperationJump/Assets/Resources/Scripts/BlockMover.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockMover : MonoBehaviour {
+
+	private float xGameArea = 7.5f;
+	private float speed = 0.5f;
+
+	private Vector3 positionLimit;
+
+	void Start() {
+		float xLength = transform.localScale.x;
+
+        if (xLength < 3.5f) {
+			positionLimit = new Vector3((xGameArea - xLength) / 2f, transform.position.y, 0);
+
+			if (Random.value <= 0.5f) {
+				positionLimit.x *= -1;
+			}
+		} else {
+			Destroy(this);
+		}
+    }
+
+	private void FixedUpdate() {
+		float movementDelta = speed * Time.deltaTime;
+
+		if (transform.position.x - positionLimit.x < 0.001f && positionLimit.x < 0) {
+			positionLimit.x = Mathf.Abs(positionLimit.x);
+		} else if (positionLimit.x - transform.position.x < 0.0001f && positionLimit.x > 0) {
+			positionLimit.x = -1 * Mathf.Abs(positionLimit.x);
+		}
+
+		transform.position = Vector3.MoveTowards(transform.position, positionLimit, movementDelta);
+	}
+}
--- OperationJump/Assets/Resources/Scripts/BlockSpawner.cs
+++ OperationJump/Assets/Resources/Scripts/BlockSpawner.cs
@@ -13,6 +13,9 @@ public class BlockSpawner : MonoBehaviour
 	private float floorY = -8f;
 	private float spawnDistance = 2.4f;
 
+	private float nextDifficultyIncrease;
+	private float difficultyIncreaseDistance;
+
 	private int blockSpawnCount;
 
 	private CameraController game;
@@ -21,30 +24,38 @@ public class BlockSpawner : MonoBehaviour
 		game = GetComponent<CameraController>();
 		nextYSpawn = floorY + spawnDistance;
 		blockSpawnCount = 0;
+
+		//20 blocks to increase difficulty
+		difficultyIncreaseDistance = 20 * spawnDistance;
+		nextDifficultyIncrease = nextYSpawn + difficultyIncreaseDistance;
 	}
 
 	void Update() {
         if (game.isGame) {
 			if (transform.position.y - floorY > nextYSpawn) {
 				// Spawn a new block
-				Vector3 newBlockPosition = new Vector3 (Random.Range(-3.75f + ((avgBlockLength + 0.5f) / 2), 3.75f - ((avgBlockLength + 0.5f) / 2)), nextYSpawn, 0);
-				GameObject newBlock = Instantiate(Block, newBlockPosition, Quaternion.identity);
-
-				Vector3 newBlockScale;
 				if (blockSpawnCount % 3 != 0) {
-					newBlockScale = new Vector3(Random.Range(avgBlockLength - 0.5f, avgBlockLength + 0.5f), 1, 1);
+					Vector3 newBlockPosition = new Vector3(Random.Range(-3.75f + ((avgBlockLength + 0.5f) / 2), 3.75f - ((avgBlockLength + 0.5f) / 2)), nextYSpawn, 0);
+					GameObject newBlock = Instantiate(Block, newBlockPosition, Quaternion.identity);
+					Vector3 newBlockScale = new Vector3(Random.Range(avgBlockLength - 0.5f, avgBlockLength + 0.5f), 1, 1);
+					newBlock.transform.localScale = newBlockScale;
 				} else {
-					newBlockScale = new Vector3(4, 1, 1);
+					Vector3 newBlockPosition = new Vector3(Random.Range(-1.75f, 1.75f), nextYSpawn, 0);
+					GameObject newBlock = Instantiate(Block, newBlockPosition, Quaternion.identity);
+					Vector3 newBlockScale = new Vector3(4, 1, 1);
+					newBlock.transform.localScale = newBlockScale;
 				}
-				
-				newBlock.transform.localScale = newBlockScale;
 
 				nextYSpawn += spawnDistance;
 				blockSpawnCount++;
 
 				// 10% chance to reduce avg block length by 0.25
-				if (Random.Range(0f, 1f) <= 0.1f) {
+				//if (Random.Range(0f, 1f) <= 0.1f) {
+				//	avgBlockLength = Mathf.Max(avgBlockLength - 0.25f, minBlockLength);
+				//}
+				if (nextDifficultyIncrease <= nextYSpawn) {
 					avgBlockLength = Mathf.Max(avgBlockLength - 0.25f, minBlockLength);
+					nextDifficultyIncrease += difficultyIncreaseDistance;
 				}
 			}
 		}
